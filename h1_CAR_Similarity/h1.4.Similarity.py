ba8a2cadb9ebe6e891f68f08705c4beaec0a70e9
LAPTOP-5H515OKA\yoonseok <yoontjr@gmail.com>
2021-10-13 14:02:19 +0900

    사이킷런 모듈 활용 코사인 유사도 계산 Pickle로 저장된 sparse matrix를 가져와서, 라인별로 비교 → 비교 조건(대상 연도 동일 AND 법인등록번호 동일) 비교 조건에 부합하지 않아 비교되지 않은 건 999 입력후 제거 추출

---
+++ h1_CAR_Similarity/h1.4.Similarity.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 11 10:57:09 2021
+
+@author: yoonseok
+"""
+
+import os
+import pandas as pd
+import pickle
+from tqdm import tqdm
+import numpy as np
+
+from sklearn.metrics.pairwise import cosine_similarity
+
+# data preprocessing
+os.chdir(r"C:\data")
+
+df = pd.read_csv("h1.preprocessedDocumentsOut_FS.txt")
+
+df["documents"].str.strip()  # Whitespace 제거
+df["count"] = df["documents"].str.len()  # string 수 
+df = df[df["count"] > 100]  # 입력내용이 100을 초과하는 입력값   
+
+with open("h1.tfIdfVec_twitter_noun_trigram.pkl", 'rb') as inp:
+    vectors = pickle.load(inp)
+
+df["vector"] = [np.reshape(x, (1, vectors.shape[1])) for x in tqdm(vectors.toarray())]
+
+df = df.drop(["documents"], axis=1)
+del vectors, inp
+
+df_ind = pd.read_excel("industry.xlsx", dtype={'KSIC': str}, sheet_name='data')
+df = df.rename(columns={"11": "INDUSTRY"})
+
+df = pd.merge(df, df_ind, on = "INDUSTRY", how ='left')
+
+df["floatYE"] = [float(x.replace("(","").replace(")","")) for x in df["5"]]
+
+simList = []
+simList.append(999)
+
+for number in tqdm(range(1, len(df))):
+    sim = cosine_similarity(df["vector"][number], df["vector"][number-1])
+    if df["10"][number] == df["10"][number-1] and df["floatYE"][number] == df["floatYE"][number-1]+1 :
+        sim = cosine_similarity(df["vector"][number], df["vector"][number-1])
+        simList.append(sim[0][0])
+    else:
+        simList.append(999)
+
+df["similarity"] = simList
+
+dfYear = df[["key", "5", "10", "KSIC", "count", "similarity"]]
+dfYear = dfYear[dfYear["similarity"] != 999]
+
+dfYear.to_csv("h1.similarity.txt")
