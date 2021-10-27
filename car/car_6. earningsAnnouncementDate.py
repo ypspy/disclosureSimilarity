c9fc324f8d1794634999dc1755dd5b6fc48475f9
LAPTOP-5H515OKA\yoonseok <yoontjr@gmail.com>
2021-10-27 22:53:21 +0900

    코드 수정 - 기재정정을 버리면 정정 전이 남아있는 것이 아니기 때문에 기재정정을 남긴다. - car 매핑 키를 따로 생성한다.

--- car/car_6. earningsAnnouncementDate.py
+++ car/car_6. earningsAnnouncementDate.py
@@ -12,21 +12,21 @@ import os
 os.chdir(r"C:\data\car\\")
 
 # 기본 테이블 입력
-df = pd.read_csv("car_1_reportTypeI_list.csv", sep='\t')
+df = pd.read_csv("car_1_reportTypeI_list.csv")
 del df["Unnamed: 0"]
 
 # 백복현 등 (2012)에 따라 잠정실적/주총소집/매출액또는손익구조 변경 중 가장 빠른 날
 
 df1 = df[df["report_nm"].str.contains("(잠정)실적", regex=False) == True]
 df1 = df1[df1["report_nm"].str.contains("(자회사의 주요경영사항)", regex=False) == False]
-df1 = df1[df1["report_nm"].str.contains("기재정정", regex=False) == False]
+# df1 = df1[df1["report_nm"].str.contains("기재정정", regex=False) == False]
 
 df2 = df[df["report_nm"].str.contains("주주총회소집결의", regex=False) == True]
-df2 = df2[df2["report_nm"].str.contains("기재정정", regex=False) == False]
+# df2 = df2[df2["report_nm"].str.contains("기재정정", regex=False) == False]
 
 df3 = df[df["report_nm"].str.contains("매출액또는손익구조", regex=False) == True]
 df3 = df3[df3["report_nm"].str.contains("(자회사의 주요경영사항)", regex=False) == False]
-df3 = df3[df3["report_nm"].str.contains("기재정정", regex=False) == False]
+# df3 = df3[df3["report_nm"].str.contains("기재정정", regex=False) == False]
 
 result = pd.concat([df1, df2, df3])
 result = result.sort_values(by=["stock_code", "rcept_dt"])
@@ -49,11 +49,11 @@ result = result[result["drop"] == True]
 key = []
 for number in range(len(result)):
     key.append(str(result.iloc[number,2].astype(int)) + str(result.iloc[number,9]-1))
-result["key"] = key
+result["carKey"] = key
 
 # 가장 빠른 날만 남기고 제거
-result = result.drop_duplicates(subset=["key"])
+result = result.drop_duplicates(subset=["carKey"])
 
 # 종목코드 이익공시일 추출
-result2 = result[["key", "rcept_dt"]]
+result2 = result[["carKey", "year", "rcept_dt"]]
 result2.to_csv("car_2_earningsAccouncementDate.csv")
